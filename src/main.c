aa52ea662f98bf40097a1ba1607778c24bac13b1
Thibault AUVRAY <tauvray@student.42.fr>
2015-02-28 10:21:52 +0100

    Test

--- src/main.c
+++ src/main.c
@@ -6,14 +6,24 @@
 /*   By: anouvel <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/02/27 21:21:42 by anouvel           #+#    #+#             */
-/*   Updated: 2015/02/27 21:22:20 by anouvel          ###   ########.fr       */
+/*   Updated: 2015/02/27 22:40:28 by tauvray          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "wong.h"
+#include <ncurses.h>
+#include <unistd.h>
+#define DELAY 30000
+
 
 int main (void)
 {
-	ft_putendl("Hello world");
-	return (0);
+	initscr();
+	noecho();
+	curs_set(FALSE);
+	mvprintw(0, 0, "Hello, world!");
+	refresh();
+	sleep(1);
+	endwin();
+return (0);
 }
